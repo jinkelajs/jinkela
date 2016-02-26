aa3aabde50bf112348923271956e99aecffd4a7d
YanagiEiichi <admin@web-tinker.com>
2016-02-26 22:04:38 +0800

    Optimize

--- jinkela.js
+++ jinkela.js
@@ -12,19 +12,15 @@ document.currentScript === void 0 && Object.defineProperty(document, 'currentScr
   }
 });
 
-// Create a unique hash
-var createUUID = function() {
-  return Date.now().toString(36) + '' + Math.floor(Math.random() * Math.pow(36, 10)).toString(36);
-};
+// To generate a unique id
+var createId = function() { return createId.i = createId.i + 1 || 1; };
 
 // Walk the tree and change "{xxx}" template to accessor properties.
 var parseTempalte = function(that) {
-  var cache = {};
   var watches = Object.create(null);
   // Walking and match special templates into "watches"
   void function callee(node) {
-    var name, attrs, child, handler;
-    var i;
+    var attrs, child, handler, attr, i;
     // Try to match directive
     if (directives[node.nodeName]) {
       handler = directives[node.nodeName](that, node);
@@ -36,59 +32,54 @@ var parseTempalte = function(that) {
         }
       }
     }
-    // Try to match binding node
+    // Try to match binding node and save if matched
     if (typeof node.nodeValue === 'string' && node.nodeValue.match(/^\{([$_a-zA-Z][$\w]*)\}$/g)) {
-      name = RegExp.$1;
-      (name in watches ? watches[name] : watches[name] = []).push(handler || node);
+      (RegExp.$1 in watches ? watches[RegExp.$1] : watches[RegExp.$1] = []).push(handler || node);
     }
-    var attr;
+    // Traversing as a binary tree
     if (attrs = node.attributes) for (i = 0; attr = attrs[i]; i++) callee(attr);
-    if (child = node.firstChild) {
-      callee(child);
-      for (var sibling = child; sibling = sibling.nextSibling; callee(sibling));
-    }
+    if (child = node.firstChild) do { callee(child); } while (child = child.nextSibling);
   }(that.element);
   // Change "watches" to accessor properties
   for (var name in watches) void function(name) {
     var list = watches[name];
     var value = that[name];
+    var cache;
     Object.defineProperty(that, name, {
+      get: function() { return cache; },
       set: function(value) {
-        cache[name] = value;
+        cache = value;
         for (var i = 0; i < list.length; i++) {
           if (typeof list[i] === 'function') {
-            list[i](value);
+            list[i].call(that, value);
           } else {
             list[i].nodeValue = value;
           }
         }
-      },
-      get: function() { return cache[name]; }
+      }
     });
     that[name] = value;
   }(name);
 };
 
-// Some element require specifial parent element
-var strictTagMap = { td: 'tr', 'th': 'tr', tr: 'tbody', tbody: 'table', thead: 'table', tfoot: 'table' };
-
-// To build and cache instance tempalte
+// To build and cache instance tempalte and return an element instance
+var buildTempalteTagMap = { td: 'tr', 'th': 'tr', tr: 'tbody', tbody: 'table', thead: 'table', tfoot: 'table' };
 var buildTempalte = function(that) {
   var target = that.constructor;
   if (!target.jinkela) {
     var template = that.template || '<div></div>';
+    // Some element require specifial parent element
     var tagName = String(template.replace(/<!--[\s\S]*?-->/g, '').match(/<([a-z][\w-]*)|$/i)[1]).toLowerCase();
     // Build template
-    target.jinkela = document.createElement(strictTagMap[tagName] || 'jinkela');
+    target.jinkela = document.createElement(buildTempalteTagMap[tagName] || 'jinkela');
     target.jinkela.innerHTML = template;
-    if (target.jinkela.children.length !== 1) {
-      throw new Error('Jinkela: Template require ☝️ and only ☝️ root element');
-    }
-    target.jinkela = target.jinkela.children[0];
-    var classId = createUUID();
-    target.jinkela.setAttribute('jinkela-class', classId);
+    if (target.jinkela.children.length !== 1) throw new Error('Jinkela: Template require 1 root element');
+    target.jinkela = target.jinkela.firstElementChild;
+    // Build styleSheet as a style tag
     var styleSheet = that.styleSheet;
     if (styleSheet) {
+      var classId = createId();
+      target.jinkela.setAttribute('jinkela-class', classId);
       styleSheet = styleSheet.replace(/:scope\b/g, '[jinkela-class="' + classId + '"]');
       document.documentElement.firstChild.insertAdjacentHTML('beforeend', '<style>' + styleSheet + '</style>');
     }
@@ -96,18 +87,9 @@ var buildTempalte = function(that) {
   return target.jinkela.cloneNode(true);
 };
 
-// Init public and private properties
-var initProperties = function(that) {
-  Object.defineProperties(that, {
-    uuid: { value: createUUID() },
-    element: { value: buildTempalte(that) }
-  });
-  /* that.element.setAttribute('jinkela-id', that.uuid); */
-};
-
 // Main Constructor
-var Jinkela = function(a) {
-  initProperties(this);
+var Jinkela = function() {
+  Object.defineProperty(this, 'element', { value: buildTempalte(this) });
   parseTempalte(this);
   if (typeof this.init === 'function') this.init();
 };
@@ -118,19 +100,15 @@ var createRender = function(name, handler) {
   Object.defineProperty(Jinkela.prototype, name, {
     value: function() {
       handler.apply(this, arguments);
-      for (var i = 0; i < this.didMountHandlers.length; i++) {
-        this.didMountHandlers[i].call(this);
-      }
+      for (var i = 0; i < this.didMountHandlers.length; i++) this.didMountHandlers[i].call(this);
       return this;
     }
   });
 };
 createRender('renderHere', function() {
-  var currentScript = document.currentScript;
-  if (!currentScript || !currentScript.parentNode) {
-    throw new Error('Jinkela: I don\'t know where are you 🙈');
-  }
-  currentScript.parentNode.insertBefore(this.element, currentScript);
+  var element = document.currentScript;
+  if (!element || !element.parentNode) throw new Error('Jinkela: "renderHere" must call synchronously');
+  element.parentNode.insertBefore(this.element, element);
 });
 createRender('renderTo', function(target) {
   if (target instanceof Jinkela) target = target.element;
@@ -142,7 +120,7 @@ createRender('renderWith', function(target) {
 });
 
 // Directive register
-var directives = {};
+var directives = Object.create(null);
 var directivesR = [];
 Jinkela.register = function(type, factory) {
   if (type instanceof RegExp) {
