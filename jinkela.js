b75ff4a2d16d67d9db124663ab993132ca8bcf6e
YanagiEiichi <admin@web-tinker.com>
2016-03-05 13:04:15 +0800

    Copy automatically from raw

--- jinkela.js
+++ jinkela.js
@@ -99,9 +99,11 @@ var buildTempalte = function(that) {
 };
 
 // Main Constructor
-var Jinkela = function() {
+var Jinkela = function(raw) {
   Object.defineProperty(this, 'element', { value: buildTempalte(this) });
   parseTempalte(this);
+  // Copy properties from raw
+  if (raw instanceof Object) for (var i in raw) this[i] = raw[i];
   // Find all "init" method list in prototype chain and call they
   var list = [];
   for (var i = this; i; i = Object. getPrototypeOf(i)) {
