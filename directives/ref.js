87c9a93e01f85da5df225f7a9088085c7631552f
YanagiEiichi <admin@web-tinker.com>
2016-03-05 15:59:47 +0800

    Make "ref" directive writable

--- directives/ref.js
+++ directives/ref.js
@@ -1,3 +1,17 @@
 Jinkela.register('ref', function(that, node, ownerElement) {
   that[node.value] = ownerElement;
+  Object.defineProperty(that, node.value, {
+    configurable: true,
+    enumerable: true,
+    get: function() { return ownerElement; },
+    set: function(element) {
+      if (element instanceof Jinkela) element = element.element;
+      var parent = ownerElement.parentNode;
+      if (parent) {
+        parent.insertBefore(element, ownerElement);
+        parent.removeChild(ownerElement);
+      }
+      ownerElement = element;
+    }
+  });
 });
