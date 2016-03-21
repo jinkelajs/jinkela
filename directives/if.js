5f9824c500ee38a3034765eb5e8fd2d57cd2c1b0
YanagiEiichi <admin@web-tinker.com>
2016-03-21 13:34:54 +0800

    Add if-not support

--- directives/if.js
+++ directives/if.js
@@ -1,19 +1,21 @@
-Jinkela.register('if', function(that, node, ownerElement) {
-  var replacement = document.createComment(' if ');
+Jinkela.register(/^if(-not)?$/, function(that, node, ownerElement) {
+  var not = !!RegExp.$1;
+  var replacement = new Comment(' ' + node.name + '="' + node.value + '" ');
   var state = true;
   var name = /^\{(.*)\}$|$/.exec(node.value)[1];
   that.didMountHandlers.push(function() { this[name] = this[name]; });
   return function(value) {
-    if (state === !!value) return;
+    value = !!value ^ not;
+    if (state === value) return;
     if (value) {
       if (replacement.parentNode) {
         replacement.parentNode.replaceChild(ownerElement, replacement);
-        state = !!value;
+        state = value;
       }
     } else {
       if (ownerElement.parentNode) {
         ownerElement.parentNode.replaceChild(replacement, ownerElement);
-        state = !!value;
+        state = value;
       }
     }
   };
