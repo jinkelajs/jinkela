110e14068dd0970b4aacc6fb44382b3ca95f77c0
YanagiEiichi <admin@web-tinker.com>
2016-05-08 01:21:06 +0800

    Optimize

--- jinkela.js
+++ jinkela.js
@@ -1,18 +1,11 @@
 /**/ 'use strict';
 /**/ void function() { /**/
 
-// To store the jinkela-class in styleSheet
-var head = document.documentElement.firstChild;
-var getStylePool = function() {
-  if (getStylePool.cache) return getStylePool.cache;
-  var style = document.createElement('style');
-  head.appendChild(style);
-  return getStylePool.cache = style;
-};
+var STRICT_TAG = { td: 'tr', 'th': 'tr', tr: 'tbody', tbody: 'table', thead: 'table', tfoot: 'table' };
+var NODE_TYPE_NAME = { 2: 'value', 3: 'data' };
+var inc = 1;
 
-// To generate a unique id
-var createId = function() { return createId.i = createId.i + 1 || 1; };
-
+// Utils Defination
 var getShadedProps = function(that, propName) {
   var list = [];
   for (var i = that; i; i = Object.getPrototypeOf(i)) {
@@ -21,9 +14,16 @@ var getShadedProps = function(that, propName) {
   }
   return list;
 };
+var getOnce = function(base, name, getter) {
+  Object.defineProperty(base, name, {
+    configurable: true,
+    get: function() {
+      return Object.defineProperty(this, name, { value: getter.call(this) })[name];
+    }
+  });
+};
 
 // Walk the tree and change "{xxx}" template to accessor properties.
-var parseTempaltePropMap = { 2: 'value', 3: 'data' };
 var parseTempalte = function(that) {
   var watches = Object.create(null);
   // Walking and match special templates into "watches"
@@ -41,7 +41,7 @@ var parseTempalte = function(that) {
       }
     }
     // Try to match binding node (textNode or attrNode) and save if matched
-    if (/^\{([$_a-zA-Z][$\w]*)\}$/.test(node[parseTempaltePropMap[node.nodeType]])) {
+    if (/^\{([$_a-zA-Z][$\w]*)\}$/.test(node[NODE_TYPE_NAME[node.nodeType]])) {
       (RegExp.$1 in watches ? watches[RegExp.$1] : watches[RegExp.$1] = []).push(handler || node);
     }
     // Traversing as a binary tree
@@ -61,7 +61,7 @@ var parseTempalte = function(that) {
           if (typeof list[i] === 'function') {
             list[i].call(that, value);
           } else {
-            list[i][parseTempaltePropMap[list[i].nodeType]] = value;
+            list[i][NODE_TYPE_NAME[list[i].nodeType]] = value;
           }
         }
       }
@@ -70,37 +70,8 @@ var parseTempalte = function(that) {
   }(name);
 };
 
-// To build and cache instance tempalte and return an element instance
-var buildTempalteTagMap = { td: 'tr', 'th': 'tr', tr: 'tbody', tbody: 'table', thead: 'table', tfoot: 'table' };
-var buildTempalte = function(that) {
-  var target = that.constructor;
-  if (!target.hasOwnProperty('jinkela')) {
-    var template = that.template || '<div></div>';
-    // Some element require specifial parent element
-    var tagName = String(template.replace(/<!--[\s\S]*?-->/g, '').match(/<([a-z][\w-]*)|$/i)[1]).toLowerCase();
-    // Build template
-    target.jinkela = document.createElement(buildTempalteTagMap[tagName] || 'jinkela');
-    target.jinkela.innerHTML = template;
-    if (target.jinkela.children.length !== 1) throw new Error('Jinkela: Template require 1 root element');
-    target.jinkela = target.jinkela.firstElementChild;
-    // Build styleSheet as a style tag
-    var styleSheet = that.styleSheet;
-    // Find all shaded "styleSheet" from prototype chain
-    var styleSheetList = getShadedProps(that, 'styleSheet');
-    if (styleSheetList.length) {
-      var classId = createId();
-      target.jinkela.setAttribute('jinkela-class', classId);
-      styleSheet = styleSheetList.reverse().join('\n').replace(/:scope\b/g, '[jinkela-class="' + classId + '"]');
-      if (typeof Jinkela.cssPreprocessor === 'function') styleSheet = Jinkela.cssPreprocessor(styleSheet);
-      getStylePool().insertAdjacentHTML('beforeend', styleSheet);
-    }
-  }
-  return target.jinkela.cloneNode(true);
-};
-
 // Main Constructor
 var Jinkela = function() {
-  Object.defineProperty(this, 'element', { value: buildTempalte(this) });
   parseTempalte(this);
   // Extends each arguments to this
   for (var i = 0; i < arguments.length; i++) {
@@ -112,15 +83,38 @@ var Jinkela = function() {
   while (list.length) list.pop().apply(this, arguments);
 };
 
-// Method Definations
-Object.defineProperty(Jinkela.prototype, 'didMountHandlers', {
-  configurable: true,
-  get: function() {
-    var value = [];
-    Object.defineProperty(this, 'didMountHandlers', { value: value });
-    return value;
+// Prototype Properties
+getOnce(Jinkela.prototype, 'element', function() {
+  var target = this.constructor;
+  if (!target.hasOwnProperty('jinkela')) {
+    var template = this.template || '<div></div>';
+    // Some element require specifial parent element
+    var tagName = String(template.replace(/<!--[\s\S]*?-->/g, '').match(/<([a-z][\w-]*)|$/i)[1]).toLowerCase();
+    // Build template
+    target.jinkela = document.createElement(STRICT_TAG[tagName] || 'jinkela');
+    target.jinkela.innerHTML = template;
+    if (target.jinkela.children.length !== 1) throw new Error('Jinkela: Template require 1 root element');
+    target.jinkela = target.jinkela.firstElementChild;
+    // Build styleSheet as a style tag
+    var styleSheet = this.styleSheet;
+    // Find all shaded "styleSheet" from prototype chain
+    var styleSheetList = getShadedProps(this, 'styleSheet');
+    if (styleSheetList.length) {
+      var classId = inc++;
+      target.jinkela.setAttribute('jinkela-class', classId);
+      styleSheet = styleSheetList.reverse().join('\n').replace(/:scope\b/g, '[jinkela-class="' + classId + '"]');
+      if (typeof Jinkela.cssPreprocessor === 'function') styleSheet = Jinkela.cssPreprocessor(styleSheet);
+      Jinkela.style.insertAdjacentHTML('beforeend', styleSheet);
+    }
   }
+  return target.jinkela.cloneNode(true);
 });
+getOnce(Jinkela.prototype, 'didMountHandlers', function() { return []; });
+getOnce(Jinkela, 'style', function() {
+  return document.documentElement.firstChild.appendChild(document.createElement('style'));
+});
+
+// Method Definations
 var createRender = function(name, handler) {
   Object.defineProperty(Jinkela.prototype, name, {
     value: function() {
