28f6c68a02ce18288aa0d9fabb8dd067b3ea6df1
YanagiEiichi <admin@web-tinker.com>
2016-11-10 15:40:08 +0800

    Add list support for ref

--- directives/ref.js
+++ directives/ref.js
@@ -1,22 +1,53 @@
 Jinkela.register('ref', function(that, node, ownerElement) {
+  var fixNode = function(item) {
+    if (item == null) item = new Comment(' ' + item + ' ');
+    if (item instanceof Jinkela) item = item.element;
+    if (!(item instanceof Node)) item = new Text(item);
+    return item;
+  };
   // var desc = Object.getOwnPropertyDescriptor(that, node.value);
   // TODO: Consider shaded props
   Object.defineProperty(that, node.value, {
     configurable: true,
     enumerable: true,
     get: function() {
-      return ownerElement.component || ownerElement;
+      if (ownerElement instanceof DocumentFragment) {
+        return ownerElement.originalList;
+      } else {
+        return ownerElement.component || ownerElement;
+      }
     },
     set: function(element) {
-      if (element == null) element = new Comment(' ' + element + ' ');
-      if (element instanceof Jinkela) element = element.element;
-      if (!(element instanceof Node)) element = new Text(element);
-      var parent = ownerElement.parentNode;
-      if (parent) {
-        parent.insertBefore(element, ownerElement);
-        parent.removeChild(ownerElement);
+      if (element instanceof HTMLCollection || element instanceof NodeList) element = [].slice.call(element);
+      if (element instanceof DocumentFragment) element = [].slice.call(element.childNodes);
+      if (element instanceof Array) {
+        if (element.length) {
+          var fragment = new DocumentFragment();
+          element.forEach(item => fragment.appendChild(fixNode(item)));
+          fragment.originalList = element;
+          element = fragment;
+        } else {
+          element = new Comment(' empty list ');
+        }
+      } else {
+        element = fixNode(element);
       }
-      ownerElement = element;
+      if (ownerElement instanceof DocumentFragment) {
+        var first = ownerElement.originalList[0];
+        var parent = first.parentNode;
+        if (parent) {
+          parent.insertBefore(element, first);
+          ownerElement.originalList.forEach(item => item.remove());
+        }
+        ownerElement = element;
+      } else {
+        var parent = ownerElement.parentNode;
+        if (parent) {
+          parent.insertBefore(element, ownerElement);
+          ownerElement.remove();
+        }
+        ownerElement = element;
+      }
     }
   });
 });
