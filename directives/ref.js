5ed44062be6f3f16c7614e410afc033b66612ba3
YanagiEiichi <admin@web-tinker.com>
2017-10-10 19:58:22 +0800

    Update ref

--- directives/ref.js
+++ directives/ref.js
@@ -4,6 +4,7 @@ Jinkela.register({
   priority: 20,
 
   handler: function(that, node, ownerElement) {
+
     var fixNode = function(item) {
       if (item == null) item = document.createComment(' ' + item + ' '); // eslint-disable-line
       if (item instanceof Node && item.component) item = item.component;
@@ -11,52 +12,39 @@ Jinkela.register({
       if (!(item instanceof Node)) item = document.createTextNode(item);
       return item;
     };
-    // var desc = Object.getOwnPropertyDescriptor(that, node.value);
-    // TODO: Consider shaded props
-    Object.defineProperty(that, node.value, {
+
+    var name = node.value;
+
+    var desc;
+    for (var i = that; i && !desc; i = Object.getPrototypeOf(i)) desc = Object.getOwnPropertyDescriptor(i, name);
+
+    var storage = { originalList: [ ownerElement ] };
+    Object.defineProperty(that, name, {
       configurable: true,
       enumerable: true,
-      get: function() {
-        if (ownerElement instanceof DocumentFragment) {
-          return ownerElement.originalList;
-        } else {
-          return ownerElement.component || ownerElement;
+      get: function() { return 'originalValue' in storage ? storage.originalValue : ownerElement.component || ownerElement; },
+      set: function(what) {
+        if ('originalValue' in storage && storage.originalValue === what) return;
+        var list = [ document.createComment(' ref="' + name + '" ') ].concat(what).map(fixNode);
+        var parent = storage.originalList[0].parentNode;
+        if (parent) {
+          for (var i = 0; i < list.length; i++) parent.insertBefore(list[i], storage.originalList[0]);
+          for (var i = 0; i < storage.originalList.length; i++) parent.removeChild(storage.originalList[i]);
         }
-      },
-      set: function(element) {
-        if (element instanceof HTMLCollection || element instanceof NodeList) element = [].slice.call(element);
-        if (element instanceof DocumentFragment) element = [].slice.call(element.childNodes);
-        if (element instanceof Array) {
-          if (element.length) {
-            var fragment = document.createDocumentFragment();
-            element.forEach(function(item) { fragment.appendChild(fixNode(item)); });
-            fragment.originalList = element;
-            element = fragment;
-          } else {
-            element = document.createComment(' empty list ');
-          }
-        } else {
-          element = fixNode(element);
-        }
-        var parent;
-        if (ownerElement instanceof DocumentFragment) {
-          var first = ownerElement.originalList[0];
-          if (parent = first.parentNode) {
-            parent.insertBefore(element, first);
-            ownerElement.originalList.forEach(function(item) {
-              if (item.parentNode) item.parentNode.removeChild(item);
-            });
-          }
-          ownerElement = element;
-        } else if (element !== ownerElement) {
-          if (parent = ownerElement.parentNode) {
-            parent.insertBefore(element, ownerElement);
-            if (ownerElement.parentNode) ownerElement.parentNode.removeChild(ownerElement);
-          }
-          ownerElement = element;
-        }
+        storage.originalValue = what;
+        storage.originalList = list;
+        if (desc && desc.set) desc.set.call(that, what);
       }
     });
+
+    that['@@beforeInit'].push(function() {
+      if (desc && desc.get) {
+        that[name] = desc.get.call(that);
+      } else {
+        if (desc && desc.value !== void 0) that[name] = desc.value;
+      }
+    });
+
   }
 
 });
