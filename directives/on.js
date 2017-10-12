87a8ee8256624c0c6a22c8e1a861e5ee5b21b266
YanagiEiichi <admin@web-tinker.com>
2017-10-12 10:46:14 +0800

    Replace ownerElement.component to @@binding for on.js

--- directives/on.js
+++ directives/on.js
@@ -4,18 +4,23 @@ Jinkela.register({
   priority: 100,
 
   handler: function(that, node, ownerElement) {
-    var eventName = node.nodeName.match(/^on-(.*)|$/)[1];
-    // Register a beforeInit handler
+
     that['@@beforeInit'].push(function() {
-      if (ownerElement.component) ownerElement = ownerElement.component.element;
+
+      var eventName = node.nodeName.match(/^on-(.*)|$/)[1];
+      var list = ownerElement['@@binding']; // Get once element list in timing @@beforeInit
+
       var handler = typeof node.jinkelaValue === 'function' && node.jinkelaValue.bind(that);
-      if (handler) ownerElement.addEventListener(eventName, handler);
+      if (handler) list.forEach(function(element) { element.addEventListener(eventName, handler); });
+
       node['@@subscribers'].push(function() {
-        if (handler) ownerElement.removeEventListener(eventName, handler);
+        if (handler) list.forEach(function(element) { element.removeEventListener(eventName, handler); });
         handler = typeof node.jinkelaValue === 'function' && node.jinkelaValue.bind(that);
-        if (handler) ownerElement.addEventListener(eventName, handler);
+        if (handler) list.forEach(function(element) { element.addEventListener(eventName, handler); });
       });
+
     });
+
   }
 
 });
